3431cdc4ce35c953248a72511764a727f0c39ab2
Dabeast44 <samsonma44@gmail.com>
2024-03-14 01:23:14 -0400

    added basic select command

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +18,19 @@ public class Main {
             }else{
                 System.out.println("Connection Unsuccessful");
             }
+
+            Statement statement = connection.createStatement();
+            statement.execute("SELECT * FROM students");
+            ResultSet resultSet = statement.getResultSet();
+            while(resultSet.next()){
+                System.out.print(resultSet.getInt("student_id") + "\t");
+                System.out.print(resultSet.getString("first_name") + "\t" );
+                System.out.print(resultSet.getString("last_name") + "\t");
+                System.out.print(resultSet.getString("email") + "\t");
+                System.out.println(resultSet.getString("enrollment_date"));
+            }
+
+
 
         }catch (Exception e){
             System.out.println(e.getMessage());
